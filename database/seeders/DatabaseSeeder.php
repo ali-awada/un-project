9abceda4ec85e63130534e7c6bed1bf10977c454
Derar Sattouf <derar.satouf@toothpickapp.com>
2022-05-28 22:18:43 +0300

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,9 @@ class DatabaseSeeder extends Seeder
         \App\Models\Currency::factory(10)->create();
         \App\Models\Category::factory(10)->create();
         \App\Models\Brand::factory(10)->create();
-//        \App\Models\Product::factory(40)->create()->each(function ($product) {
-//            $product->images()->saveMany(\App\Models\ProductImage::factory(4)->make());
-//        });
+        \App\Models\Product::factory(40)->create()->each(function ($product) {
+            $product->images()->saveMany(\App\Models\ProductImage::factory(4)->make());
+        });
 
 //
 //        \App\Models\User::factory()->create([
